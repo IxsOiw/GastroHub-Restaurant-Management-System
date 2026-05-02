72e98c0a8dde0cce12a9d7310445a673e559fd1f
IxsOiw <tomas.labos@student.ukf.sk>
2026-05-02 22:39:18 +0200

    Refactor: remove unused DB dependency from HomeController

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -2,17 +2,8 @@
 
 namespace Ixsaiw\Bistro\Controllers;
 
-use Ixsaiw\Bistro\Database;
-
 class HomeController
 {
-    protected $db;
-
-    public function __construct($config)
-    {
-        $this->db = new Database($config['database']);
-    }
-
     public function index()
     {
         $heading = "Home";
