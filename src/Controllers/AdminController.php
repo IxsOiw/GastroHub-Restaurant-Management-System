d162c16a7c7f95f675d55d4b8968b4eed9209655
IxsOiw <tomas.labos@student.ukf.sk>
2026-05-03 23:22:38 +0200

    add reservations to admin dashboard

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -2,13 +2,29 @@
 
 namespace Ixsaiw\Bistro\Controllers;
 
+use Ixsaiw\Bistro\Database;
+
 class AdminController
 {
+    protected Database $db;
+
+    public function __construct($config)
+    {
+        $this->db = new Database($config['database']);
+    }
+
     public function index()
     {
+
         if (empty($_SESSION['admin'])) {
             redirect('/admin-login');
         }
+
+        $reservations = $this->db->getAll(
+            "SELECT * FROM reservation ORDER BY date DESC, timing DESC LIMIT 5"
+        );
+
+
         $heading = "Admin";
         require __DIR__ . '/../../views/admin.view.php';
     }
--- views/partials/reservations-table.php
+++ views/partials/reservations-table.php
@@ -4,51 +4,22 @@
           <a href="/admin/rezervacie" class="btn btn-outline btn-sm">Všetky</a>
         </div>
         <table class="admin-table">
-          <thead>
-            <tr>
-              <th>Hosť</th>
-              <th>Čas</th>
-              <th>Osoby</th>
-              <th>Stav</th>
-            </tr>
-          </thead>
           <tbody>
-            <tr>
-              <td>
-                <span class="table-avatar">JK</span>
-                Jana Kováč
-              </td>
-              <td>18:00</td>
-              <td>2</td>
-              <td><span class="badge badge-confirmed">Potvrdená</span></td>
-            </tr>
-            <tr>
-              <td>
-                <span class="table-avatar">MN</span>
-                Martin Novák
-              </td>
-              <td>19:30</td>
-              <td>4</td>
-              <td><span class="badge badge-pending">Čaká</span></td>
-            </tr>
-            <tr>
-              <td>
-                <span class="table-avatar">EH</span>
-                Eva Horváth
-              </td>
-              <td>20:00</td>
-              <td>3</td>
-              <td><span class="badge badge-cancelled">Zrušená</span></td>
-            </tr>
-            <tr>
-              <td>
-                <span class="table-avatar">PB</span>
-                Peter Blaho
-              </td>
-              <td>20:30</td>
-              <td>6</td>
-              <td><span class="badge badge-pending">Čaká</span></td>
-            </tr>
+            <?php foreach ($reservations as $reservation): ?>
+              <tr>
+                <td>
+                  <span class="table-avatar">
+                    <?= strtoupper(substr($reservation['name'], 0, 2)) ?>
+                  </span>
+                  <?= htmlspecialchars($reservation['name']) ?>
+                </td>
+                <td><?= htmlspecialchars($reservation['timing']) ?></td>
+                <td><?= htmlspecialchars($reservation['people']) ?></td>
+                <td>
+                  <span class="badge badge-confirmed">Potvrdená</span>
+                </td>
+              </tr>
+            <?php endforeach; ?>
           </tbody>
         </table>
       </div>
