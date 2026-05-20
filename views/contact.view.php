e69808a51be4e03bcb2e4d4d3a48d5a6a3d8cb76
IxsOiw <tomas.labos@student.ukf.sk>
2026-05-20 17:48:43 +0200

    add: information

--- views/contact.view.php
+++ views/contact.view.php
@@ -6,12 +6,15 @@
 
 <?php require __DIR__ . '/partials/hero.php'; ?>
 
-<main>
-  <div>
-  <p>Hello to contact page</p>
+<section class="tw-flex tw-w-full tw-place-content-center tw-place-items-center tw-gap-[10%] tw-overflow-hidden tw-bg-[#EFEFEF] tw-p-4 tw-px-[10%] max-md:tw-flex-col">
+  <div class="tw-mt-[5%] tw-flex tw-h-full tw-flex-col tw-gap-[5%]">
+    <h2 class="primary-text-color tw-text-3xl tw-font-medium">Contact us</h2>
+    <h3 class="tw-text-5xl max-md:tw-text-3xl">Get in touch</h3>
+    <p class="tw-mt-4 tw-text-gray-600">📍 2 Lord Edward St, D02 P634</p>
+    <p class="tw-text-gray-600">📞 +123 232 123</p>
+    <p class="tw-text-gray-600">✉️ info@bistro.sk</p>
   </div>
-</main>
-
+</section>
 
 <?php require __DIR__ . '/partials/newsletter.php'; ?>
 <?php require __DIR__ . '/partials/footer.php'; ?>
